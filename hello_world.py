bd17ef388f9896c74c51e1d357b071abd70ac74a
Muttley <Margi896@gmail.com>
2026-03-04 23:42:18 +0000

    variables init

--- hello_world.py
+++ hello_world.py
@@ -8,6 +8,38 @@ print("Python")
 print("Programming", "Essentials","in",sep="***",end="...")
 print("Python.",)
 
+#variables - learning variables and how to use them
+#strings - learning how to use strings and format them
+
 first_name = "muttley"
 print(first_name)
 
+print("My name is", first_name)
+
+print(f"Hello, {first_name}!")
+
+email = "muttley@example.com"
+print(f"My email is {email}.")
+
+favourite_food = "mini egg cookies"
+
+#strings 
+age = 101
+quantity = 7
+
+print(f"I am {age} years old")
+print (f"I need to buy {quantity} {favourite_food}")
+
+#float
+
+price = 3.500
+distance = 21.1
+
+print(f"The price of {favourite_food} is £{price}")
+print(f"I am running a half marathon which is {distance}km")
+
+#Boolean (funny name) - May be used in If statements
+
+lil_bitch = True
+
+print(f"Is patrick a lil bitch? {lil_bitch}")
